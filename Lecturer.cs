8f40bce884236765bf2a5f6383c29a800f17230c
scotad2 <scotad2@student.op.ac.nz>
2026-08-13 14:44:37 +1200

    feat: add enums to lecturer class

--- Lecturer.cs
+++ Lecturer.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace WinFormsApp1
 {
-    internal class Lecturer
+    internal class Lecturer : Person
     {
+        public enum EPosition
+        {
+            Lecturer = 0,
+            Senior_Lecturer = 1,
+            Principal_Lecturer = 2,
+            Associate_Professor = 3,
+            Professor = 4
+        }
+
+        public enum ESalary
+        {
+            Lecturer_Salary = 85000,
+            Senior_Lecturer_Salary = 100000,
+            Principal_Lecturer_Salary = 115000,
+            Associate_Professor_Salary = 130000,
+            Professor_Salary = 145000
+        }
     }
 }
