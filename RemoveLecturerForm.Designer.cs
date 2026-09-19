4a7a3168614ed929691b91c110cc5bc9d897ec92
scotad2 <scotad2@student.op.ac.nz>
2026-09-19 15:40:53 +1200

    feat: add RemoveLecturerForm.cs

---
+++ RemoveLecturerForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WinFormsApp1
+{
+    partial class RemoveLecturerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            IdTextBox = new TextBox();
+            CancelButton = new Button();
+            DeleteButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Lecturer ID:";
+            // 
+            // IdTextBox
+            // 
+            IdTextBox.Location = new Point(12, 27);
+            IdTextBox.Name = "IdTextBox";
+            IdTextBox.Size = new Size(100, 23);
+            IdTextBox.TabIndex = 1;
+            // 
+            // CancelButton
+            // 
+            CancelButton.Location = new Point(12, 56);
+            CancelButton.Name = "CancelButton";
+            CancelButton.Size = new Size(75, 23);
+            CancelButton.TabIndex = 2;
+            CancelButton.Text = "Cancel";
+            CancelButton.UseVisualStyleBackColor = true;
+            CancelButton.Click += CancelButton_Click;
+            // 
+            // DeleteButton
+            // 
+            DeleteButton.BackColor = Color.Tomato;
+            DeleteButton.Location = new Point(93, 56);
+            DeleteButton.Name = "DeleteButton";
+            DeleteButton.Size = new Size(75, 23);
+            DeleteButton.TabIndex = 3;
+            DeleteButton.Text = "Delete";
+            DeleteButton.UseVisualStyleBackColor = false;
+            DeleteButton.Click += DeleteButton_Click;
+            // 
+            // RemoveLecturerForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(175, 90);
+            Controls.Add(DeleteButton);
+            Controls.Add(CancelButton);
+            Controls.Add(IdTextBox);
+            Controls.Add(label1);
+            Name = "RemoveLecturerForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox IdTextBox;
+        private Button CancelButton;
+        private Button DeleteButton;
+    }
+}
---
+++ RemoveLecturerForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class RemoveLecturerForm : Form
+    {
+        private DataHandler _dataHandler;
+
+        internal RemoveLecturerForm(DataHandler dataHandler)
+        {
+            InitializeComponent();
+
+            _dataHandler = dataHandler;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(IdTextBox.Text);
+
+            _dataHandler.RemoveLecturer(id);
+
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
